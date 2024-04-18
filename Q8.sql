028cb50829bd66c50f952e3eaa4efc0d90fd60ae
Sanjeev Nayak <45550700+snjvnyk@users.noreply.github.com>
2024-04-18 11:56:20 -0700

    Update Q8.sql

--- Q8.sql
+++ Q8.sql
@@ -1,5 +1,11 @@
-SELECT zipcode, gender, SUM(population) AS total_population
-FROM `bigquery-public-data.census_bureau_usa.population_by_zip_2010`
-GROUP BY zipcode, gender
-ORDER BY SUM(population) DESC
-LIMIT 2;
+WITH female_population_by_zip AS (
+    SELECT zipcode, SUM(population) AS total_female_population,
+           RANK() OVER (ORDER BY SUM(population) DESC) AS zipcode_rank
+    FROM `bigquery-public-data.census_bureau_usa.population_by_zip_2010`
+    WHERE gender = 'female'
+    GROUP BY zipcode
+)
+
+SELECT zipcode, total_female_population
+FROM female_population_by_zip
+WHERE zipcode_rank <= 5;

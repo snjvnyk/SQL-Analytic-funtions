d949937e79d74f1f99cc8d0dfef873d603e32c77
Sanjeev Nayak <45550700+snjvnyk@users.noreply.github.com>
2024-04-19 10:55:52 -0700

    Update Q6.sql

--- Q6.sql
+++ Q6.sql
@@ -1,5 +1,12 @@
-SELECT gender, MIN(minimum_age) AS min_age, MAX(maximum_age) AS max_age, SUM(population) AS head_count
-FROM `bigquery-public-data.census_bureau_usa.population_by_zip_2010`
-GROUP BY gender
-ORDER BY SUM(population) DESC
-LIMIT 5;
+WITH zip_population AS (
+    SELECT 
+        zipcode,
+        gender,
+        SUM(population) AS total_population,
+        RANK() OVER (PARTITION BY gender ORDER BY SUM(population) DESC) AS population_rank
+    FROM `bigquery-public-data.census_bureau_usa.population_by_zip_2010`
+    GROUP BY zipcode, gender
+)
+SELECT zipcode, gender, total_population
+FROM zip_population
+WHERE population_rank = 1;

6fcf14d4972556874cdf8b42d8b15e7b5af78619
Sanjeev Nayak <45550700+snjvnyk@users.noreply.github.com>
2024-04-19 10:55:29 -0700

    Update Q5.sql

--- Q5.sql
+++ Q5.sql
@@ -1,6 +1,12 @@
-SELECT zipcode, SUM(population) AS total_female_population
-FROM `bigquery-public-data.census_bureau_usa.population_by_zip_2010`
-WHERE gender = 'female'
-GROUP BY zipcode
-ORDER BY SUM(population) DESC
-LIMIT 5;
+WITH female_population_by_age AS (
+    SELECT
+        MAX(maximum_age) AS max_age,
+        SUM(population) AS female_population,
+        RANK() OVER (ORDER BY SUM(population) DESC) AS population_rank
+    FROM `bigquery-public-data.census_bureau_usa.population_by_zip_2010`
+    WHERE zipcode = '94085' AND gender = 'female'
+    GROUP BY maximum_age
+)
+SELECT max_age, female_population
+FROM female_population_by_age
+WHERE population_rank = 1;

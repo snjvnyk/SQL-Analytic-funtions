c7fe76665b4952def800749273fcf4a587cb1b43
Sanjeev Nayak <45550700+snjvnyk@users.noreply.github.com>
2024-04-19 10:54:48 -0700

    Update Q4.sql

--- Q4.sql
+++ Q4.sql
@@ -1,5 +1,12 @@
-SELECT zipcode, gender, SUM(population) AS total_population
-FROM `bigquery-public-data.census_bureau_usa.population_by_zip_2010`
-GROUP BY zipcode, gender
-ORDER BY SUM(population) DESC
-LIMIT 2;
+WITH male_population_by_age AS (
+    SELECT
+        MAX(maximum_age) AS max_age,
+        SUM(population) AS male_population,
+        RANK() OVER (ORDER BY SUM(population) DESC) AS population_rank
+    FROM `bigquery-public-data.census_bureau_usa.population_by_zip_2010`
+    WHERE zipcode = '94085' AND gender = 'male'
+    GROUP BY maximum_age
+)
+SELECT max_age, male_population
+FROM male_population_by_age
+WHERE population_rank = 1;
